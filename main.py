dd96b852794c6809bbf6678116e53553fb44e17e
Khoa <62637453+KarenYuusha@users.noreply.github.com>
2025-07-12 21:41:32 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from module.core.cursor import switch_to_toram
+from module.event.anniv import battle, to_battle
+from time import sleep
+from tqdm import trange
+
+switch_to_toram('normal')
+for _ in trange(1000):
+    to_battle()
+    battle()
+    sleep(2)
