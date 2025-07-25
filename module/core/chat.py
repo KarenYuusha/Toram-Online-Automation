3df301e1da8775ae59c76f02a008f962b091a61a
KarenYuusha <Phitieutrongdat@gmail.com>
2025-07-25 14:58:44 +0700

    minor update

--- module/core/chat.py
+++ module/core/chat.py
@@ -4,22 +4,24 @@ from time import sleep
 import pyautogui as gui
 from dotenv import load_dotenv
 from pushbullet import Pushbullet
+import keyboard
 
 dir = os.path.dirname(__file__)
 dotenv_path = os.path.join(dir, '..', '..', 'asset', 'API', '.env')
 load_dotenv(dotenv_path)
 
+
 def send_sms_message(title, text) -> None:
     """
     Send a notification message using Pushbullet.
-    
+
     Parameters:
         title (str): The title of the notification.
         text (str): The content of the notification.
-    
+
     Requires the Pushbullet API key (`PUSHBULLET_API_KEY`) in asset/API/.env file.
     """
-    
+
     # If KEY is none then print out on terminal
     KEY = os.getenv('PUSHBULLET_API_KEY')
     if KEY is None:
@@ -28,3 +30,9 @@ def send_sms_message(title, text) -> None:
 
     pb = Pushbullet(KEY)
     pb.push_note(title, text)
+
+
+def type_text(text, delay=0.1):
+    for char in text:
+        keyboard.write(char)
+        sleep(delay)
